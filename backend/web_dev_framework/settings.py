a08ec0f223c359e6af04963bdc9d56f34261deb0
Lucas Vervloessem <lucas.vervloessem.atc@gmail.com>
2025-01-07 17:44:45 +0100

    feat(setup): django-rest-framework

--- backend/web_dev_framework/settings.py
+++ backend/web_dev_framework/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
 ]
 
 MIDDLEWARE = [
@@ -103,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "fr-fr"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "Etc/GMT-1"
 
 USE_I18N = True
 
