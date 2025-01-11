9d9a45cfbea17581c7dd8aac44fd2bbce9b34b69
Lucas Vervloessem <lucas.vervloessem.atc@gmail.com>
2025-01-11 12:36:31 +0100

    feat(user): registering user

--- backend/user/views.py
+++ backend/user/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
-from rest_framework.decorators import api_view
-from django.http import HttpResponse
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import UserSerializer
 
 # Create your views here.
-def hello(request):
-    return HttpResponse("hello world")
+class User(APIView):
+    def post(self, request):
+        serializer: UserSerializer = UserSerializer(data=request.data) 
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.validated_data, status.HTTP_200_OK)
+        return Response(serializer.errors, status.HTTP_400_BAD_REQUEST)
+        
